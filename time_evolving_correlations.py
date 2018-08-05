5f3e282bf5191bd4b4d03c2672d268c4d227180a
annierak <annie1rak@gmail.com>
2018-08-04 18:19:56 -0700

    more features for time correlation plotting: select by stimulus, corr_matrix muscle labels, flexible display and computation time windows, muscle pair legend

--- time_evolving_correlations.py
+++ time_evolving_correlations.py
@@ -10,18 +10,22 @@ import matplotlib.cm
 
 
 fly_num = 1542
+groups_of_interest = ['hg']
+
+frames_per_step = 5
+disp_window_size = 5
+corr_window_size = 1
+duration = 10*60
+start_at_stimulus = 'cl_blocks, g_x=-1, g_y=4, b_x=8, b_y=0, ch=True'
 
 try:
     fly = flb.NetFly(fly_num,rootpath='/home/annie/imager/media/imager/FlyDataD/FlyDB/')
 except(OSError):
     fly = flb.NetFly(fly_num)
 
-
-# fly = flb.NetFly(1538,rootpath='/home/annie/work/programming/fly_muscle_data/')
-
 flydf = fly.construct_dataframe()
-# flydf = flydf[0:5000]
-# print(flydf.columns.values)
+
+
 muscle_cols = \
 ['iii1_left', 'iii3_left',
  'i1_left',  'i2_left',
@@ -33,7 +37,6 @@ muscle_cols = \
  'b1_right', 'b2_right', 'b3_right' ]
 
 
-groups_of_interest = ['hg','i']
 filtered_muscle_cols = [
 [muscle for muscle in muscle_cols if (
 (group in muscle[0:len(group)]) and (muscle[len(group)+1]!='i'))]
@@ -44,27 +47,25 @@ num_groups = len(groups_of_interest)
 filtered_muscle_cols = list(itertools.chain(*filtered_muscle_cols))
 muscle_count = len(filtered_muscle_cols)
 
-
-
-frames_per_step = 5
 dt = frames_per_step*(flydf['t'][1]-flydf['t'][0])
-window_size = 3
-window_size = np.floor(window_size/dt)*dt
-time.sleep(1)
-t_stop = 10*60
+corr_window_size = np.floor(corr_window_size/dt)*dt
+disp_window_size = np.floor(disp_window_size/dt)*dt
 
-# plt.figure(2)
-# plt.plot(flydf['t'])
-# plt.show()
-# time.sleep(5)
-# plt.show()
-t = 0
-counter = 0
 
-time_window_inds = (flydf['t']>t)&(flydf['t']<=t+window_size)
+stimulus_inds = flydf['stimulus']==start_at_stimulus
+
+print(type(flydf['t'][stimulus_inds]))
+print(flydf['t'][stimulus_inds][0:5])
+print(flydf['t'][stimulus_inds][0:5].keys())
+
+t = float(flydf['t'][stimulus_inds][0:1].tolist()[0])
+counter = flydf['t'][stimulus_inds][0:1].keys()[0]
+print(t,counter)
+
+time_window_inds = (flydf['t']>t)&(flydf['t']<=t+corr_window_size)
 muscle_matrix = np.array(flydf.loc[time_window_inds,filtered_muscle_cols]).T
-print(np.shape(muscle_matrix))
-steps_per_window = int(window_size/dt)
+steps_per_disp_window = int(disp_window_size/dt)
+steps_per_corr_window = int(corr_window_size/dt)
 
 muscle_pair_array = util.prod_list(filtered_muscle_cols,square=True)
 
@@ -72,14 +73,15 @@ muscle_pair_list = util.symm_matrix_half(muscle_pair_array)
 group_index_list = [length*[index] for index,length in enumerate(group_lengths_list) ]
 
 colormap1= matplotlib.cm.get_cmap('RdYlBu_r')
-# colormap2 = matplotlib.cm.get_cmap('Dark2')
 colormap2 = ['blue', 'orange', 'green', 'red',
  'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
 num_colors = (num_groups*(num_groups+1)/2)
 color_list = np.zeros(np.shape(muscle_pair_list)[0]).astype(int)
 
-group_pair_list = util.prod_list(groups_of_interest)
-print(group_pair_list)
+group_pair_list = util.symm_matrix_half(util.prod_list(groups_of_interest,square=True),exclude_diagonal=False)
+group_pair_list = [tuple(pair) for pair in group_pair_list]
+# print(group_pair_list)
+# raw_input(' ')
 
 for ind,pair in enumerate(muscle_pair_list):
     chopped_pair = tuple(util.muscle_group(pair))
@@ -98,60 +100,60 @@ for ind,pair in enumerate(muscle_pair_list):
 
 plt.ion()
 ax1 = plt.subplot(1,2,1)
-# fig.canvas.flush_events()
-title_text =plt.title(' ')
+title_text =ax1.set_title(' ',fontsize=2)
 
-corr_values_by_t = np.zeros((muscle_count*(muscle_count+1)/2-muscle_count,steps_per_window))
+corr_values_by_t = np.zeros((muscle_count*(muscle_count+1)/2-muscle_count,steps_per_disp_window))
 lines = plt.plot(corr_values_by_t.T)
 for ind,line in enumerate(lines):
     line.set_color(colormap2[color_list[ind]])
-plt.ylim([-160,180])
+corr_range = [-60,60]
+plt.ylim(corr_range)
+colors_used = [colormap2[unique_color] for unique_color in np.unique(color_list)]
+custom_lines = [matplotlib.lines.Line2D([0], [0], color=c, lw=4) for c in colors_used]
+plt.legend(custom_lines, group_pair_list,loc=4,bbox_to_anchor=(1.1,0.,0.6,0.5))
 ax2 = plt.subplot(1,2,2)
-image = plt.imshow(np.zeros((22,22)),vmin=-150,vmax=150,interpolation='none',
+image = plt.imshow(np.zeros((len(
+    filtered_muscle_cols),len(filtered_muscle_cols))),vmin=corr_range[0],vmax=corr_range[1],interpolation='none',
 cmap=colormap1)
-# ax3 = plt.subplot(3,1,3)
-# n,bins,_ = plt.hist(np.zeros(22*22))
-# raw_input('--')
+ax2.yaxis.set_ticks_position('left')
+ax2.xaxis.set_ticks_position('top')
+plt.xticks(range(len(filtered_muscle_cols)),rotation=90)
+plt.yticks(range(len(filtered_muscle_cols)))
+ax2.set_xticklabels(filtered_muscle_cols)
+ax2.set_yticklabels(filtered_muscle_cols)
+ax2.set_position([0.55,0.35,0.45,0.45])
 
-while t<t_stop:
+while t<duration:
+    # print(counter)
     # print(t)
-    time_window_inds = (flydf['t']>t)&(flydf['t']<=t+window_size)
+    time_window_inds = (flydf['t']>t)&(flydf['t']<=t+corr_window_size)
     state_mtrx = np.array(flydf.loc[time_window_inds,filtered_muscle_cols]).T
     text = flydf.iloc[counter]['stimulus']
-    ax1.set_title(text)
+    ax1.set_title(text,fontsize=12)
     # time.sleep(5)
 
     #Watch out for muscles that have no activity
     off_muscle_inds = (np.sum(state_mtrx,axis=1)==0.)
     # Set them to a very small amt of activity so nan's are not created
     state_mtrx[off_muscle_inds] = 1e-8
-    # print(state_mtrx[4,1:50])
     centered_mtrx = state_mtrx - np.mean(state_mtrx,axis = 1)[:,None]
-    # print(np.std(centered_mtrx,axis = 1))
     std_mtrx = centered_mtrx/np.std(centered_mtrx,axis = 1)[:,None]
-    # print(std_mtrx[np.isnan(std_mtrx)])
     cor_mtrx = np.dot(std_mtrx,std_mtrx.T)
-
     image.set_data(cor_mtrx)
-    # ax3.cla()
-    # print(np.unique((cor_mtrx.flatten())))
-    # plt.hist((cor_mtrx.flatten()))
-
     corr_values = util.symm_matrix_half(cor_mtrx)
 
-    if counter<steps_per_window:
+    if counter<steps_per_disp_window:
         corr_values_by_t[:,counter] = corr_values
     else:
-        # print('past starting phase')
         corr_values_by_t[:,:-1] = corr_values_by_t[:,1:]
         corr_values_by_t[:,-1] = corr_values
-        x_values = np.linspace(t,t+window_size,steps_per_window)
-        ax1.set_xlim((t,t+window_size))
+        x_values = np.linspace(t,t+disp_window_size,steps_per_disp_window)
+        ax1.set_xlim((t,t+disp_window_size))
         for row,line in enumerate(lines):
             line.set_ydata(corr_values_by_t[row,:])
             line.set_xdata(x_values)
             line.set_color(colormap2[color_list[row]])
     plt.draw()
-    plt.pause(0.001)
+    plt.pause(0.01)
     t+=dt
     counter+=1
