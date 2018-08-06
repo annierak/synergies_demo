4d6ee30c2fc3e409445d7546483bb35c00070e99
annierak <annie1rak@gmail.com>
2018-08-06 13:43:55 -0700

    right left filter, colors within muscle groups

--- time_evolving_correlations.py
+++ time_evolving_correlations.py
@@ -9,8 +9,9 @@ import time
 import matplotlib.cm
 
 
-fly_num = 1542
-groups_of_interest = ['hg']
+fly_num = 1545
+groups_of_interest = ['i','b']
+side_of_interest = 'left' #Specify 'both' for both
 
 frames_per_step = 5
 disp_window_size = 5
@@ -26,7 +27,7 @@ except(OSError):
 flydf = fly.construct_dataframe()
 
 
-muscle_cols = \
+filtered_muscle_cols = \
 ['iii1_left', 'iii3_left',
  'i1_left',  'i2_left',
  'hg1_left', 'hg2_left', 'hg3_left', 'hg4_left',
@@ -36,11 +37,18 @@ muscle_cols = \
  'hg1_right', 'hg2_right', 'hg3_right', 'hg4_right',
  'b1_right', 'b2_right', 'b3_right' ]
 
+ #filter for L/R
+if side_of_interest != 'both':
+ filtered_muscle_cols = \
+ [muscle for muscle in filtered_muscle_cols if util.muscle_side(muscle)==side_of_interest]
 
+#filter for muscle type
 filtered_muscle_cols = [
-[muscle for muscle in muscle_cols if (
+[muscle for muscle in filtered_muscle_cols if (
 (group in muscle[0:len(group)]) and (muscle[len(group)+1]!='i'))]
 for group in groups_of_interest]
+
+
 group_lengths_list = [len(group_list) for group_list in filtered_muscle_cols]
 num_groups = len(groups_of_interest)
 
@@ -73,23 +81,33 @@ muscle_pair_list = util.symm_matrix_half(muscle_pair_array)
 group_index_list = [length*[index] for index,length in enumerate(group_lengths_list) ]
 
 colormap1= matplotlib.cm.get_cmap('RdYlBu_r')
-colormap2 = ['blue', 'orange', 'green', 'red',
- 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
+if len(groups_of_interest)>1:
+    single_mucle_mode = True
+    colormap2 = ['blue', 'orange', 'green', 'red',
+     'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
+else:
+    single_mucle_mode = False
+    pair_count = muscle_count*(muscle_count+1)/2 - muscle_count
+    cmap = matplotlib.cm.get_cmap('Paired',pair_count)
+    colormap2 = [cmap(ind) for ind in np.linspace(0,1,pair_count)]
 num_colors = (num_groups*(num_groups+1)/2)
-color_list = np.zeros(np.shape(muscle_pair_list)[0]).astype(int)
 
 group_pair_list = util.symm_matrix_half(util.prod_list(groups_of_interest,square=True),exclude_diagonal=False)
 group_pair_list = [tuple(pair) for pair in group_pair_list]
 # print(group_pair_list)
 # raw_input(' ')
 
-for ind,pair in enumerate(muscle_pair_list):
-    chopped_pair = tuple(util.muscle_group(pair))
-    try:
-        color = group_pair_list.index(chopped_pair)
-    except(ValueError):
-        color = group_pair_list.index((chopped_pair[1],chopped_pair[0]))
-    color_list[ind] = int(color)
+if single_mucle_mode:
+    color_list = np.zeros(np.shape(muscle_pair_list)[0]).astype(int)
+    for ind,pair in enumerate(muscle_pair_list):
+        chopped_pair = tuple(util.muscle_group(pair))
+        try:
+            color = group_pair_list.index(chopped_pair)
+        except(ValueError):
+            color = group_pair_list.index((chopped_pair[1],chopped_pair[0]))
+        color_list[ind] = int(color)
+else:
+    color_list = range(pair_count)
 
 
 #Test the muscle pair colors
@@ -99,6 +117,7 @@ for ind,pair in enumerate(muscle_pair_list):
 
 
 plt.ion()
+plt.figure(figsize=(11,8))
 ax1 = plt.subplot(1,2,1)
 title_text =ax1.set_title(' ',fontsize=2)
 
@@ -110,7 +129,12 @@ corr_range = [-60,60]
 plt.ylim(corr_range)
 colors_used = [colormap2[unique_color] for unique_color in np.unique(color_list)]
 custom_lines = [matplotlib.lines.Line2D([0], [0], color=c, lw=4) for c in colors_used]
-plt.legend(custom_lines, group_pair_list,loc=4,bbox_to_anchor=(1.1,0.,0.6,0.5))
+if single_mucle_mode:
+    legend_labs = group_pair_list
+else:
+    legend_labs = muscle_pair_list
+plt.legend(custom_lines, legend_labs,loc=4,bbox_to_anchor=(1.6,-0.1  ,0.6,0.5),
+    fontsize=10,ncol=2)
 ax2 = plt.subplot(1,2,2)
 image = plt.imshow(np.zeros((len(
     filtered_muscle_cols),len(filtered_muscle_cols))),vmin=corr_range[0],vmax=corr_range[1],interpolation='none',
@@ -121,7 +145,7 @@ plt.xticks(range(len(filtered_muscle_cols)),rotation=90)
 plt.yticks(range(len(filtered_muscle_cols)))
 ax2.set_xticklabels(filtered_muscle_cols)
 ax2.set_yticklabels(filtered_muscle_cols)
-ax2.set_position([0.55,0.35,0.45,0.45])
+ax2.set_position([0.55,0.40,0.45,0.45])
 
 while t<duration:
     # print(counter)
