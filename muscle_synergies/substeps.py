cb161ea2ef4519afc43d9fe564ae97d6ba4d7bcb
annierak <annie1rak@gmail.com>
2018-08-29 17:45:41 -0700

    part 3 working superficially

--- muscle_synergies/substeps.py
+++ muscle_synergies/substeps.py
@@ -95,7 +95,7 @@ def squared_error_gradient_episode(M_s,c_s,W,delays_s):
 	# M_s: D x T muscle activity for that episode
 	# c_s: N synergy coefficents
 	# W : N x D x T synergies
-	# delay_s : N delays 
+	# delay_s : N delays
 	D,T = np.shape(M_s)
 	nabla = np.zeros_like(c_s)
 	N = len(c_s)
@@ -113,29 +113,41 @@ def squared_error_gradient_total(M,c,W,delays,i,tao):
 	#Computes the gradient of (sum_s) E^2 wrt entries of w_i_tao--
 	#(each dimension of w_i_tao is a muscle activity value)
 	#Inputs:
-	# M: S x D x T muscle activity 
+	# M: S x D x T muscle activity
 	# c: S x N synergy coefficents
 	# W : N x D x T synergies
-	# delay : S x N delays 
+	# delay : S x N delays
 
 	S,D,T = np.shape(M)
 	nabla = np.zeros(D)
 	_,N = np.shape(delays)
-	
+
+	#The two sections immediately below should be made more efficient with object implementation
+
+	#first, initialize a shifted W so we don't have to compute it again each time
+	#produce a matrix S x N x D x T of the Ws for each s,j shift pair
+	shifted_W = np.array([[shift_matrix_columns(delays[s,j],W[j,:,:]) for j in range(
+		N)] for s in range(S)])
+	#for every s,j pair scale the shifted W
+	scaled_shifted_W = c[:,:,None,None]*shifted_W
+	#then sum across each j
+	summed_scaled_shifted_W = np.sum(scaled_shifted_W,axis=1)
+
+	#make a tao/i indicator matrix of shape S x N x T
+	tao_i_indicator = np.array([(tao==t-delays).astype(int) for t in range(T)]) #this could be further de-looped
+	tao_i_indicator = np.moveaxis(tao_i_indicator,0,2)
+	#and scale it by its coefficent in the gradient expression (c_s_j)
+	scaled_tao_i_indicator = c[:,:,None]*tao_i_indicator
+	#and sum accross each j
+	summed_scaled_tao_i_indicator = np.sum(scaled_tao_i_indicator,axis=1)
+
 	for d_0 in range(D):
-		values_by_s_by_t = np.zeros((S,T)) 
-		for t in range(T):
-			for s in range(S):
-				# second_term,third_term = np.zeros(N),np.zeros(N)
-				second_term = np.array([c[s,j]*shift_matrix_columns(delays[s,j],W[j,:,:])[d_0,t] for j in range(N)])
-				third_term = np.array([c[s,j]*(tao==t-delays[s,j]) for j in range(N)])
-				entry = (2*M[s,d_0,t] - np.sum(second_term))*(-1)*np.sum(third_term)
-				values_by_s_by_t[s,t] = entry 
+		values_by_s_by_t = (2*M[:,d_0,:] - summed_scaled_shifted_W[:,d_0,:])*(-1)*summed_scaled_tao_i_indicator
 		value = np.sum(values_by_s_by_t)
 		nabla[d_0] = value
 	return nabla
 
-		
+
 
 def compute_squared_error(W,c,t,M):
 	#Returns the sum squared error across episodes as defined at the top of section 3
@@ -147,5 +159,3 @@ def compute_squared_error(W,c,t,M):
 			entries_by_d = M[s,:,t]-np.sum(W[:,:,t]*c[s,:][:,None],axis=0)
 			error[s,t] = np.sum(np.square(entries_by_d))
 	return np.sum(error)
-
-	 
